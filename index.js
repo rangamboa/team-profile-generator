bbb7390a5effaebc82aac2ebb81003a99d0e5926
rangamboa <rangamboa@gmail.com>
2021-07-21 06:27:27 -0700

    constructor and inquirer coding

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHtml = require('./util/generateHtml.js');
-const Manager = require('./lib/Manager.js');
+//const Manager = require('./lib/Manager.js');
 
 // const generateHtml = require('./lib/Engineer.js');
 // const generateHtml = require('./lib/Intern.js');
-// const employee = require('./lib/Employee.js');
+const employee = require('./lib/Employee.js');
 
 // Boolean to keep track of whether the manager information has been entered yet or not.
 let mgrInput;
@@ -23,7 +23,7 @@ function init() {
     // Initialize this value to false the first time the app is run.
     mgrInput = false;
 
-    // Initialize employee ID nubmer to 1.
+    // Initialize employee ID number to 1.
     idCount = 1;
 
     generateEmployee();
@@ -34,62 +34,42 @@ const generateEmployee = () => {
     console.log('\n-----')
     console.log('Welcome to the Web Dev Team Profile Generator!');
 
-    inquirer
-        // Create a generic non-role employee.
-        .prompt([
-            {
-                name: 'name',
-                type: 'input',
-                message: 'Please enter your Name.',
-            },
-            {
-                name: 'email',
-                type: 'input',
-                message: 'Please enter your Email Address.',
-            },
-            {
-                name: 'role',
-                type: 'list',
-                message: 'What is your Role on the Team?',
-                choices: ['Manager', 'Engineer', 'Intern'],
-            },
-        ])
+    employee(idCount);
 
-        .then((answers) => {
+    // inquirer
+    //     // Create a generic non-role employee.
+    //     .prompt([
+    //         {
+    //             name: 'name',
+    //             type: 'input',
+    //             message: 'Please enter your Name.',
+    //         },
+    //         {
+    //             name: 'email',
+    //             type: 'input',
+    //             message: 'Please enter your Email Address.',
+    //         },
+    //         {
+    //             name: 'role',
+    //             type: 'list',
+    //             message: 'What is your Role on the Team?',
+    //             choices: ['Manager', 'Engineer', 'Intern'],
+    //         },
+    //     ])
 
-            // Set employee ID number, then increment for next employee.
-            answers.id = idCount;
-            idCount++;
+    //     .then((answers) => {
+
+    //         // Set employee ID number, then increment for next employee.
+    //         answers.id = idCount;
+    //         idCount++;
             
-            // Set universal team role to "Employee"; this is changed later.
-            answers.role = 'Employee';
+    //         // Set universal team role to "Employee"; this is changed later.
+    //         answers.role = 'Employee';
 
-            //console.log(answers);
-        });
-
-        // Check to see if manager info has been entered. If not, this employee is assumed to be the manager.
-        console.log(mgrInput);
-        if (mgrInput == false) {
-
-            // Set mgrInput to true since there is only one manager on the team.
-            mgrInput = true;
-
-            inquirer
-            .prompt([
-                {
-                    name: 'officeNum',
-                    type: 'input',
-                    message: 'Please enter your Office Number.',
-                }
-            ])
-            .then((answers) => {
-                    
-                    console.log(answers.officeNum);
-                    console.log(answers);
-                    data = answers;
-            });
-            //Manager(answers); 
-
+    //         data = answers;
+    //         console.log(data);
+    //     });
+        // console.log('data: '+data);
 };
 
 // Call init() upon application launch.
